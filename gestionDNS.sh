5945f39ffa1d467218700a1d4c576c8c50667d6a
kevinredfield69 <kevinsevillano9743@gmail.com>
2018-11-15 11:16:03 +0100

    Editando fichero de script

--- gestionDNS.sh
+++ gestionDNS.sh
@@ -4,10 +4,10 @@ if [ $1 = "-a" ]
 then
 	if [ $2 = "-dir" ]
 	then
-		echo "$3		IN	A	$4"
+		echo "$3	IN	A	$4"
 	elif [ $2 = "-alias" ]
 	then
-		echo "$3		IN	CNAME	$4"
+		echo "$3	IN	CNAME	$4"
 	fi
 elif [ $1 = "-b" ]
 then
