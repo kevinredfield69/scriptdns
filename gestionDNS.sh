a35c4911b2b9c2873f0eeaa5838744b405214c4d
kevinredfield69 <kevinsevillano9743@gmail.com>
2018-11-16 09:58:22 +0100

    Editando fichero script

--- gestionDNS.sh
+++ gestionDNS.sh
@@ -10,10 +10,10 @@ then
 		ip=`echo $4 | cut -d "." -f3,4`
 		dominio=`sed -e '/ORIGIN/ !d' $zonadirecta | cut -d " " -f2
 		echo "$3	IN	A	$4" >> $zonadirecta
-		echo "Añadido $3	IN	A	$4 en el fichero $zonadirecta"
+		echo "Añadido $3	IN	A	$4 en el fichero $zonadirecta..."
 		sleep 1
 		echo "$ip	IN	PTR	$3.$dominio" >> $zonainversa
-		echo "Añadido $ip	IN	PTR	$3.$dominio en el fichero $zonainversa"
+		echo "Añadido $ip	IN	PTR	$3.$dominio en el fichero $zonainversa..."
 		sleep 1
 		echo "Reiniciando Servidor DNS Bind9"
 		sleep 3
@@ -22,7 +22,7 @@ then
 	elif [ $2 = "-alias" ]
 	then
 		echo "$3	IN	CNAME	$4" >> $zonadirecta
-		echo "Añadido alias	$3	IN	CNAME	$4 en el fichero $zonadirecta"
+		echo "Añadido alias	$3	IN	CNAME	$4 en el fichero $zonadirecta..."
 		sleep 1
 		echo "Reiniciando Servidor DNS Bind9"
 		sleep 3
@@ -30,7 +30,7 @@ then
 	else
 		echo "Los parametros que has introducido no son correctos."
 	fi
-#elif [ $1 = "-b" ]
-#then
-#	echo "Borrando zona..."
+elif [ $1 = "-b" ]
+then
+	echo "Borrando zona..."
 fi
